508eda2b1336e1295a33ebde5847ac172a787533
Varnit-jain123 <varnitjaintj@gmail.com>
2025-05-22 04:44:15 +0000

    adding js feature

--- script.js
+++ script.js
@@ -17,3 +17,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// Scroll to top button
+  document.querySelector(".scroll-top-btn").addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
